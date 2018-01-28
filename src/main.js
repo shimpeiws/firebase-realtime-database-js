1bcefacabcf77f87a154b80b09923f0d978fc75a
shimpeiws <shimpeiws@gmail.com>
2018-01-29 00:16:46 +0900

    Post data

--- src/main.js
+++ src/main.js
@@ -2,12 +2,31 @@ import env from '../.env.js'
 import Firebase from 'firebase'
 
 const config = {
-  apiKey: env.firebaseApiKey,
-  authDomain: env.firebaseAuthDomain,
-  databaseURL: env.firebaseDatabaseURL,
-  storageBucket: env.firebaseStorageBucket
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  databaseURL: env.databaseURL,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId
 }
+
+console.info('config', config)
 
 Firebase.initializeApp(config);
 
 const database = Firebase.database();
+
+const pushData = (name, email) => {
+  database.ref().child('foo').push({
+    name, email
+  })
+}
+
+const writeUserData = (userId, name, email) => {
+  database.ref('users/' + userId).set({
+    name, email
+  })
+};
+
+pushData(2, 'foo', 'foo@example.com');
+writeUserData('2', 'foo', 'foo@example.com');
